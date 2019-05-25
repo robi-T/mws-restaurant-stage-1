c64a481c881e00f937a18af70ed6baac1c170fee
Robert Tulek <robert.tulek@gmail.com>
2019-05-25 22:21:17 +0200

    Converted the site to use responsive design and added some accessibility features

---
+++ js/sw.js
@@ -0,0 +1,23 @@
+let staticCacheName = 'mws-restaurant-cache-v1'
+
+let cacheContent = [
+    '/',
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/sw.js'
+];
