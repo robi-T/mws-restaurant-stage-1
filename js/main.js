883061ed839d48c80ecea33303028a936b633d81
Robert Tulek <robert.tulek@gmail.com>
2019-05-29 23:56:00 +0200

    Update after the project review comments.

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
     image.alt = 'Restaurant ' + restaurant.name; // added alt text for the image
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement('h2');
     name.innerHTML = restaurant.name;
     li.append(name);
 
